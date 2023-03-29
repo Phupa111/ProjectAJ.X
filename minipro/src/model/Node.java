dac873d8f757c277a35c2c0b0ed961981a7ff966
Phupa111 <santapupa111@gmail.com>
2023-03-29 17:25:01 +0700

    Comit1

---
+++ minipro/src/model/Node.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Node {
+	
+private String name;
+	
+	public Node(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+}
